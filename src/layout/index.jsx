c8b586105bcd1f87901a38dbaeb43e1dc5a721f8
alfahmist <gst.alfahmi@gmail.com>
2024-04-25 09:57:00 +0700

    add notification transition
    layout/index.jsx

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,18 +1,30 @@
 import { Link, Outlet } from 'react-router-dom';
 import cart from '../assets/cart.svg';
 import useMarketStore from '../store/marketStore';
+import { useRef } from 'react';
 
 const index = () => {
 	const { getTotalItem, notification } = useMarketStore();
+	const ref1 = useRef();
+	let a = () => {
+		let text = '';
+		setTimeout(() => {
+			text = 'top-[50px]';
+		}, 100);
+		return text;
+	};
+	console.log(a());
 	return (
 		<>
-			{notification && (
-				<div className='fixed w-full top-[50px] h-[30px] cursor-default'>
-					<div className='text-center w-[830px] bg-black mx-auto rounded-md text-white text-sm h-full leading-[30px]'>
-						1 item added to cart
-					</div>
+			<div
+				className={`fixed w-full h-[30px] cursor-default transition-all duration-500  ${
+					notification ? 'top-[100px] opacity-100' : 'top-[0] opacity-0'
+				}`}
+			>
+				<div className='text-center w-[430px] bg-black opacity-80 mx-auto rounded-md text-white text-sm h-full leading-[30px]'>
+					1 item added to cart
 				</div>
-			)}
+			</div>
 
 			<div className='max-w-[830px] mx-auto '>
 				<div className='flex flex-row gap-10 justify-between bg-red-200 items-center px-[20px] py-[10px]'>
